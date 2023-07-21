eedd8629f939c9c96befa0e2bb48418ac4a32af7
Wildani Danial Nafis <wildaninafis203@gmail.com>
2023-07-21 17:04:48 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas odit dolore dolor deserunt impedit esse. Cum, cupiditate sunt praesentium, quo unde cumque excepturi doloremque exercitationem ab in hic modi fugit?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
